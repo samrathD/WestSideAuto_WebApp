4da0941b9a14fac341f081a79081c37d647c7bdf
samrathD <samrath2004@gmail.com>
2024-02-14 14:34:26 -0800

    add dependencies

---
+++ src/main/java/com/westside/west_side_auto/WestSideAutoApplication.java
@@ -0,0 +1,13 @@
+package com.westside.west_side_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestSideAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestSideAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/westside/west_side_auto/WestSideAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westside.west_side_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestSideAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
