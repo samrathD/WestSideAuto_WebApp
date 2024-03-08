2bd0752ba30a85699bb6f8fc75d1d284d89a0bb9
EJ <ejnicolas001@gmail.com>
2024-03-07 20:00:53 -0800

    [Add] Start of session making

--- src/main/java/com/westside/west_side_auto/models/UserRepository.java
+++ src/main/java/com/westside/west_side_auto/models/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
 	ArrayList<User> findByUid(int uid);
+	ArrayList<User> findByEmailAndPassword(String name, String password);
 }
