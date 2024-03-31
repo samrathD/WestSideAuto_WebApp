c4729aadfcf5e337cd802d62cc5219b693d54b07
EJ <ejnicolas001@gmail.com>
2024-03-30 17:53:43 -0700

    [Update] Add some CSS to instagram feed

--- src/main/resources/static/JavaScript/instagram-feed.js
+++ src/main/resources/static/JavaScript/instagram-feed.js
@@ -9,26 +9,48 @@ function getInstagramFeed() {
             const feedContainer = document.getElementById('instagram-feed');
 
             data.data.forEach(post => {
-                let postLink = document.createElement('a');
-                postLink.classList.add("post-container")
-                postLink.href = post.permalink;
-                postLink.target = '_blank';
+                let postContainer = document.createElement('div');
+                postContainer.classList.add("post-container")
+                //postLink.href = post.permalink;
+                //postLink.target = '_blank';
 
                 let postImage = document.createElement('img');
                 postImage.classList.add("post-image");
                 postImage.src = post.media_url;
                 postImage.alt = post.caption;
 
+                let postTime = document.createElement('p');
+                postTime.classList.add("post-time");
+                var readableDate = new Date(post.timestamp);
+                readableDate.toLocaleDateString('en-US');
+                postTime.innerHTML = readableDate.toDateString();
+
                 let postText = document.createElement('p');
                 postText.innerHTML = post.caption;
                 postText.classList.add("post-text");
-                postLink.appendChild(postImage);
-                postLink.appendChild(postText);
-                feedContainer.appendChild(postLink);
+
+                postContainer.appendChild(postImage);
+                postContainer.appendChild(postTime);
+                postContainer.appendChild(postText);
+                feedContainer.appendChild(postContainer);
             });
         })
         .catch(error => console.error(error));
 }
 
+function getInstagramHeader(){
+  fetch(`https://graph.instagram.com/me?fields=id,username&access_token=${accessToken}`)
+    .then(response => response.json())
+    .then (data => {
+      const header = document.getElementById("instagram-header");
+
+      let username = document.getElementById("instagram-name");
+      username.innerHTML = "@" + data.username;
+
+    })
+    .catch(error => consoler.error(error));
+}
+
 // Call the function to fetch and display the Instagram feed
 getInstagramFeed();
+getInstagramHeader();
