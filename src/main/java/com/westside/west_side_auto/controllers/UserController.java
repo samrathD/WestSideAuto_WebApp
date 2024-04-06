13249c8ec5acc06d6d29e4293f62da5e07a46733
rjathan7 <rjathan1602@gmail.com>
2024-04-05 23:52:32 -0700

    changed schedule.html

--- src/main/java/com/westside/west_side_auto/controllers/UserController.java
+++ src/main/java/com/westside/west_side_auto/controllers/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -124,7 +126,6 @@ public class UserController {
 			return "/users/updateError";
 		}
 	}
-	
 }
 
 
