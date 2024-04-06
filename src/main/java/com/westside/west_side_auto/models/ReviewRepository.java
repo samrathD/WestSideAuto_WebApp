528a4cd4dc0cb2cef980928394b576600d5e1075
Arshdeep Singh <singharshdeep5530@gmail.com>
2024-04-05 18:16:48 -0700

    Added logic for submitting reviews

---
+++ src/main/java/com/westside/west_side_auto/models/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.westside.west_side_auto.models;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<review,Integer>{
+    ArrayList<review> findByTodayDate(Date todayDate);
+}
---
+++ src/main/java/com/westside/west_side_auto/models/review.java
@@ -0,0 +1,74 @@
+package com.westside.west_side_auto.models;
+
+
+
+import java.util.Date;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="reviews")
+public class review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+
+    String username;
+    Date todayDate;
+    int rating;
+    String review;
+
+    public review() {
+        this.username = "";
+        this.todayDate = new Date(0);
+        this.rating = 0;
+        this.review = "";
+    }
+
+    public review(String username, Date todayDate, int rating, String review) {
+        this.username = username;
+        this.todayDate = todayDate;
+        this.rating = rating;
+        this.review = review;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String Username) {
+        this.username = Username;
+    }
+
+    public Date getDate() {
+        return todayDate;
+    }
+
+    public void setDate(Date todayDate) {
+        this.todayDate = todayDate;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+
+
+}
