120a0a6cf7d870ad2451ace6ac03b818852cd8a5
EJ <ejnicolas001@gmail.com>
2024-04-07 22:17:35 -0700

    [Add] Start of testing

--- src/test/java/com/westside/west_side_auto/WestSideAutoApplicationTests.java
+++ src/test/java/com/westside/west_side_auto/WestSideAutoApplicationTests.java
@@ -1,14 +1,34 @@
 package com.westside.west_side_auto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import com.westside.west_side_auto.controllers.AppointmentController;
+import com.westside.west_side_auto.controllers.UserController;
 
 @SpringBootTest
 class WestSideAutoApplicationTests {
 
+	@Autowired
+	private UserController uController;
+	
+	@Autowired
+	private AppointmentController aController;
+	
 	@Test
-	void contextLoads() {
-		//testing
+	void contextLoadsUserController() throws Exception {
+		assertThat(uController).isNotNull();
 	}
+	
+	@Test
+	void contextLoadsAppointmentController() throws Exception {
+		assertThat(aController).isNotNull();
+	}
+	
+	
 
 }
