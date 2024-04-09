0bc2b9688fd65a208d8a0c3ac74245fcde5e1a98
EJ <ejnicolas001@gmail.com>
2024-04-08 18:27:21 -0700

    [Add] Wrote some user testing methods

--- src/test/java/com/westside/west_side_auto/UserControllerTest.java
+++ src/test/java/com/westside/west_side_auto/UserControllerTest.java
@@ -4,18 +4,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.westside.west_side_auto.controllers.UserController;
+import com.westside.west_side_auto.models.User;
 import com.westside.west_side_auto.models.UserRepository;
 import com.westside.west_side_auto.models.appointmentRepository;
 import com.westside.west_side_auto.service.EmailSenderService;
@@ -37,11 +49,71 @@ public class UserControllerTest {
 	@Autowired
 	private UserController uController = new UserController();
 	
+	
 	@Test
 	void shouldGoToHomePage() throws Exception{
-		this.mockMvc.perform(get("/"))
+		mockMvc.perform(get("/"))
 				.andExpect(status().isOk())
 				.andExpect(content().string(containsString("<h1 class=\"display-3\">WEST SIDE AUTOWORKS</h1>")));
+	}
+	
+	@Test
+	void shouldAddThisUser() throws Exception{
+		mockMvc.perform(post("/users/add")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(asJsonString(new User("Bob", "abc123@gmail.com", "password")))
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(redirectedUrl("/login"));
+	}
+	
+	@Test
+	void shouldBringThisUserToLoginPageWhenClickingLoginButton() throws Exception{
+		mockMvc.perform(get("/login"))
+		.andExpect(content().string(containsString("<form class=\"form\" action=\"/login\" method=\"post\">")));
+	}
+	
+//	@Test
+//	void shouldAllowThisUserToLogin() throws Exception{
+//		mockMvc.perform(post("/login")
+//			.param("email", "abc123@gmail.com")
+//			.param("password", "password"))
+//		.andExpect(content().string(containsString("<h1>Welcome <span th:text=\"${user.username}\"></span>!</h1>")));
+//		
+//	}
+	
+	@Test 
+	void userEditsTheirProfileSuccessfully() throws Exception{
+		HashMap<String, Object> sessionAttr = new HashMap<String, Object>();
+		sessionAttr.put("session_user", new User());
+		
+		mockMvc.perform(post("/users/edit")
+				.sessionAttrs(sessionAttr)
+				.param("email", "abc123@gmail.com")
+				.param("password", "testingadifferentpassword")
+				.param("confirm-password", "testingadifferentpassword"))
+			.andExpect(content().string(containsString("<h1>Profile Updated</h2>")));
+	}	
+	
+	@Test 
+	void userEditsTheirProfileWithWrongPasswords() throws Exception{
+		HashMap<String, Object> sessionAttr = new HashMap<String, Object>();
+		sessionAttr.put("session_user", new User());
+		
+		mockMvc.perform(post("/users/edit")
+				.sessionAttrs(sessionAttr)
+				.param("email", "abc123@gmail.com")
+				.param("password", "testingadifferentpassword")
+				.param("confirm-password", "notthesamepassword"))
+			.andExpect(content().string(containsString("<h1>An Error Has Occured</h2>")));
+	}	
+	
+	
+	public static String asJsonString(final Object obj) {
+	    try {
+	        return new ObjectMapper().writeValueAsString(obj);
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
 	}
 	
 }
